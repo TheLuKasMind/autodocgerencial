57af0f288e3e6bce64d11898a78e280483b1fd29
TheLuKasMind <lucasbugatib0@gmail.com>
2026-04-26 03:46:06 -0300

    Conexão SQL remoto para local
    Adicionado conexão SQL do projeto web para ser testada em projeto em ambiente local

--- gerencial/base/connection.php
+++ gerencial/base/connection.php
@@ -1,17 +1,23 @@
 <?php
 
 try {
-    $dbGeralNET = new PDO(
-        "mysql:host=localhost;dbname=luca5858_geral2;charset=utf8",
-        "luca5858_AutoDocVaa",
-        "MZeRT6aZ5Q2F4ww"
-    );
+    //$dbGeralNET = new PDO(
+    //    "mysql:host=69.6.249.13;dbname=luca5858_geral2;charset=utf8",
+    //    "luca5858_AutoDocVaa",
+    //    "MZeRT6aZ5Q2F4ww"
+    //);
 
+     $dbGeralNET = new PDO(
+         "mysql:host=localhost;dbname=luca5858_geral2;charset=utf8",
+         "luca5858_AutoDocVaa",
+         "MZeRT6aZ5Q2F4ww"
+     );
+	
     $dbGeralNET->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbGeralNET->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
-    die("Erro de conex«ªo: " . $e->getMessage());
+    die("Erro de conexÃ£o: " . $e->getMessage());
 }
 
 ?>
