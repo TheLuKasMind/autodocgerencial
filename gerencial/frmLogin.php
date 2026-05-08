becc386bc5bda497990a433f55bd8f908f9c5b32
TheLuKasMind <lucasbugatib0@gmail.com>
2026-05-08 14:49:52 -0300

    Force refresh
    - Forçando refresh na página de login.

--- gerencial/frmLogin.php
+++ gerencial/frmLogin.php
@@ -1,4 +1,8 @@
 <?php 
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 include 'base/baseFuncoes.php'; 
 require_once 'base/connection.php'; 
 
